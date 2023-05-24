479996a0a0cb0218217bf477fc4f5a3e40c35621
Pham Dat <trongdat1702@gmail.com>
2023-05-24 14:06:05 +0700

    fix responsive detail page

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -4,7 +4,7 @@ import { Children } from '../utils/interface'
 const Body = ({ children }: Children) => {
     return (
         <>
-            <div className='bg-[#0D1016] w-full h-full lg:pt-[80px] md:pt-[75px] sm:pt-[70px] pt-[65px] overflow-y-auto'>
+            <div className='bg-[#0D1016] w-screen h-screen lg:pt-[80px] md:pt-[75px] sm:pt-[70px] pt-[65px] overflow-y-auto'>
                 <Routes>{children}</Routes>
             </div>
         </>
